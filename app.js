c0d364141c1bd5c0582166612a11b3db6993f791
Μυρτώ Βασιλείου <murtov21@gmail.com>
2022-12-15 23:03:16 +0200

    feat: minor correction in code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector('.grid')
 
 for (let i=0; i<255; i++){
-    const square = document.createElement('.div')
+    const square = document.createElement('div')
     grid.appendChild(square)
 }
 
